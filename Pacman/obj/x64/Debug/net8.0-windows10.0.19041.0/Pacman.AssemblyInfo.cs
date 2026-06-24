dd2909689bb600a216f56c639fea6830b0dd0e90
Tövisháti Márk Xavér <70947713+MrDrakosss@users.noreply.github.com>
2026-06-24 14:48:39 +0200

    asd

--- Pacman/obj/x64/Debug/net8.0-windows10.0.19041.0/Pacman.AssemblyInfo.cs
+++ Pacman/obj/x64/Debug/net8.0-windows10.0.19041.0/Pacman.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pacman")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3a3508956d7d192aef5b8844d5260950d4b1946")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fae0eda769d2e341b92540d19fe314b9ea7e19b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pacman")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pacman")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
